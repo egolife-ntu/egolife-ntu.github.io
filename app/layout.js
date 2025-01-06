6cb19a691f56d34a0cb9c976a45c2ae53e6e496c
Jingkang Yang <jingkang001@e.ntu.edu.sg>
2025-01-07 02:50:44 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter, Teachers } from "next/font/google";
 import "./globals.css";
 import "@fontsource-variable/teachers";
+import Script from "next/script"; // 关键：引入 next/script
 
 // const inter = Inter({ subsets: ["latin"] });
 // const teachers = Teachers({ subsets: ["latin"] });
@@ -13,8 +14,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <body className={inter.className}>{children}</body> */}
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-SELQYCGE0Z"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SELQYCGE0Z');
+          `}
+        </Script>
+
+        {children}
+      </body>
     </html>
   );
 }
